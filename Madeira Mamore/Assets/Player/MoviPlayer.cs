fd30c26169dc8feff9493780c8205a7b43852e93
Luan <144077591+LuanWuu@users.noreply.github.com>
2024-03-11 19:32:11 -0300

    MoviPlayer
    Criação do codigo MoviPlayer, pasta Player, movimentação de joystick e teclado

---
+++ Madeira Mamore/Assets/Player/MoviPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoviPlayer : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        Keyboard();
+        JoyStick();
+    }
+    private void Keyboard()
+    {
+        float horizontalInput = Input.GetAxis("KeyBoardH");
+        float verticalInput = Input.GetAxis("KeyBoardY");
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.fixedDeltaTime;
+            rb.velocity = movement;
+        }
+
+    }
+    private void JoyStick()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.fixedDeltaTime;
+            rb.velocity = movement;
+        }
+
+    }
+}
